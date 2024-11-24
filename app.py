4943f2fe28ff52510f7b986db64e7c1fc1ba8a63
BhavaniCh11 <bhavani.ch1102@gmail.com>
2024-11-23 19:45:42 -0500

    Updated app message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, GKE CI/CD!"
+    return "Hello, GKE CI!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
